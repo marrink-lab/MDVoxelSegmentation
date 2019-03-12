00b2f84ccb9b34045ee82b04da6e6de3f57d40cb
Bart Bruininks <BartBruininks@gmail.com>
2019-03-12 16:41:02 +0100

    Fix logging for plotting and recreating previous clusters

--- settests.py
+++ settests.py
@@ -7,7 +7,8 @@ Created on Tue Mar  5 12:17:03 2019
 """
 from collections import deque
 import numpy as np
-import copy
+import matplotlib.pyplot as plt
+
 
 cluster1 = {'a1','a2','a3','a4','a5','a6','a7','a8','a9','a10'}
 cluster2 = {'b1','b2','b3','b4','b5','b6','b7','b8','b9','b10'}
@@ -65,13 +66,20 @@ frame21 = deque([cluster11,cluster12,cluster13,cluster14,cluster15,cluster16,clu
 frame22 = deque([cluster11,cluster12,cluster13,cluster14,cluster15,cluster16,cluster17])
 frame23 = deque([cluster11,cluster12,cluster13,cluster14,cluster15,cluster16,cluster17])
 
+
 test1 = {1: {'a8', 'a5', 'a6', 'a4', 'a9', 'a2', 'a10', 'a7', 'a3', 'a1'}, 2: {'b1', 'b4', 'b3', 'b5', 'b2'}, 3: {'c4', 'c5', 'c6', 'c1', 'c2', 'c3'}, 4: {'d9', 'd5', 'd6', 'd8', 'd10', 'd2', 'd7', 'd1', 'd3', 'd4'}, 5: {'e7', 'e5', 'e6', 'e1', 'e2', 'e3', 'e8', 'e4'}, 6: {'f2', 'f1', 'f3', 'f5', 'f6', 'f7', 'f9', 'f10', 'f8', 'f4'}, 7: {'g3', 'g1', 'g2'}, 9: {'b8', 'b9', 'b7', 'b10'}}
 test2 = {1: {'a8', 'a5', 'a6', 'a4', 'a9', 'a2', 'a10', 'a7', 'a3', 'a1'}, 2: {'b1', 'b4', 'b3', 'b5', 'b2'}, 3: {'c4', 'c5', 'c6', 'c1', 'c2', 'c3'}, 5: {'e7', 'e5', 'e6', 'e1', 'e2', 'e3', 'e8', 'e4'}, 6: {'f2', 'f1', 'f3', 'f5', 'f6', 'f7', 'f9', 'f10', 'f8', 'f4'},  7: {'g3', 'g1', 'g2'}, 8: {'d9', 'd5', 'd8', 'd10', 'd7', 'd4'}}
 test3 = {1: {'a8', 'a5', 'a6', 'a4', 'a9', 'a2', 'a10', 'a7', 'a3', 'a1'}, 2: {'b1', 'b4', 'b3', 'b5', 'b2'}, 3: {'c4', 'c5', 'c6', 'c1', 'c2', 'c3'}, 4: {'d9', 'd5', 'd6', 'd8', 'd10', 'd2', 'd7', 'd1', 'd3', 'd4'}, 5: {'e7', 'e5', 'e6', 'e1', 'e2', 'e3', 'e8', 'e4'},  6: {'f2', 'f1', 'f3', 'f5', 'f6', 'f7', 'f9', 'f10', 'f8', 'f4'}, 7: {'g3', 'g1', 'g2'}}
 
-def compare_clusters (clustersold,clustersnew,clusternumber,frame_number):
+deprecated_dict = {
+        8 : {'b7','b8','b9'}
+}
+
+
+
+
+def compare_clusters (clustersold,clustersnew,deprecated_clusters,clusternumber,frame_number):
     output = {}
-    changes = []
     
     for key,value in clustersold.items():
         likeness_score=index=0
@@ -84,11 +92,11 @@ def compare_clusters (clustersold,clustersnew,clusternumber,frame_number):
              if len(value) > len(value_out):
                  output[key] = value 
                  del output[index]
-                 changes.append((index,key,'m'))
+                 deprecated_clusters[index] = value_out
                  print("cluster " + str(index) + " was merged into cluster " + str(key) + " at frame " + str(frame_number))
              else:
                  print("cluster " + str(key) + " was merged into cluster " + str(index) + " at frame " + str(frame_number))   
-                 changes.append((key,index,'m'))
+                 deprecated_clusters[key] = value
         else:
             for idx, clusternew in enumerate(clustersnew):
                 temp_score = len(value.intersection(clusternew))/max(len(value),len(clusternew)) 
@@ -105,12 +113,25 @@ def compare_clusters (clustersold,clustersnew,clusternumber,frame_number):
             if temp_score > likeness_score:
                 likeness_score = temp_score
                 index = key
-        newkey = clusternumber + 1
-        clusternumber = newkey
-        output[newkey ] = clustersnew.popleft()
-        changes.append((index,newkey,'c'))
-        print ("cluster " + str(newkey) + " was created from cluster " + str(index) + " at frame " + str(frame_number))
-    return output,clusternumber,changes
+        likeness_score = 0
+        remerge = False
+        for key, value in deprecated_clusters.items():
+            
+            temp_score = len(value.intersection(clusternew))/max(len(value),len(clusternew)) 
+            if (temp_score > identity_threshold) and (temp_score > likeness_score) :
+                likeness_score = temp_score
+                index_old = key
+                remerge = True
+            
+        if remerge:
+            output[index_old] = clustersnew.popleft()
+            print ("cluster " + str(index_old) + " was recreated from cluster " + str(index) + " at frame " + str(frame_number))
+        else:
+            newkey = clusternumber + 1
+            clusternumber = newkey
+            output[newkey ] = clustersnew.popleft()
+            print ("cluster " + str(newkey) + " was created from cluster " + str(index) + " at frame " + str(frame_number))
+    return output,clusternumber,deprecated_clusters
 
     
                 
@@ -118,59 +139,51 @@ def compare_clusters (clustersold,clustersnew,clusternumber,frame_number):
                 
             
             
-if  compare_clusters(olddict1,frame21,8,1)== test1 :
+if  compare_clusters(olddict1,frame21,deprecated_dict,8,1)== test1 :
     print(" test1 ok :)")
 
-if  compare_clusters(olddict2,frame22,8,1)== test2 :
+if  compare_clusters(olddict2,frame22,{0 : set()},8,1)== test2 :
     print(" test1 ok :)")
     
-if  compare_clusters(olddict3,frame23,8,1) == test3:
-   print("test3 ok :)")
+if  compare_clusters(olddict3,frame23,{0 : set()},8,1) == test3:
+    print("test3 ok :)")
         
 
-data = np.load('/home/albert/projects/clustering/MDVoxelClustering/test_files/uint32bin-clusters.dat.npy')
+data = np.load('/coarse/bart/projects/clustering/test_files/4_transfection/clusters.npy')
 
 # single frame set creation for all sets
 #for frame in data:
-clustercount = 0
-olddict = {0 : set()}
-changelog = {}
-aliases = {0 : set()}
-#for idx, cluster in enumerate(set(data[0])):
 #    olddict[clustercount] =set(np.where(data[0] == (idx))[0])
 #for frame in data:
 #    print(set(frame))
+identity_threshold = 0.5
 
-for idx, frame in enumerate(data):
-    newcluster = deque()
-    
-    for cluster in set(frame):     
-        newcluster.append(set(np.where(frame == cluster)[0]))  
-    olddict, clustercount, changes = compare_clusters(olddict, newcluster,clustercount,idx)
-    for key, value in olddict.items():
-        for index in value:
-            frame[index] = key
+def sort_clusters(data):
+    clustercount = 0
+    olddict = {0 : set()}
+    deprecated_clusters = {}
+    plotinfo = {0 : []}
+    for idx, frame in enumerate(data):
+        newcluster = deque()
+        plotvariables = []
+        for cluster in set(frame):     
+            newcluster.append(set(np.where(frame == cluster)[0]))  
+        olddict, clustercount, deprecated_clusters = compare_clusters(olddict, newcluster, deprecated_clusters,clustercount,idx)
+        for key, value in olddict.items():
+            for index in value:
+                frame[index] = key
+            plotvariables.append((key,len(value)))
+        plotinfo[idx] = plotvariables
+    return data,plotinfo
+data, plotinfo = sort_clusters(data)
 
-
-
-#aliases = { 0 : set()}
-#replacelist = deque()
-#replacelist.append((0,[0]))
-#print(replacelist[-1][1])
-#for time, changes in changelog.items():
-#    for change in changes:
-#        if change[2] == 'm':
-#            aliases[change[0]].append((change,time))
-#            replacelist.append((time,replacelist[-1][1].remove(change[0])))
-#        elif change[2] == 'c':
-#            newclus = max(aliases,key=int)+1
-#            aliases[max(aliases,key=int)+1] = [(change,time)]   
-#            print(replacelist)
-#            replacelist.append((time,copy.copy(replacelist[-1][1]).append(newclus)))
+np.save('clusters_ordered', data)
 #
-#
-#
-#print(aliases)
-#print(replacelist)
-        
-    
+#time = range(len(plotinfo))
+#ordered_clusters = plotinfo.values()
+#print(time)
+#print(ordered_clusters) 
+#plt.bar(time, ordered_clusters, align='center')
+#plt.xticks(range(len(plotinfo)), list(plotinfo.keys()))
+#        
+plt.show()
