e8d6ce22eb685327d5243f140ee492a30c23c92b
AlbertThie <alb0rt@gmail.com>
2019-07-08 20:00:38 +0200

    add testfiles for settests

---
+++ tests/test_settests/conftest.py
@@ -0,0 +1,149 @@
+import mdvoxelclustering as mdv
+from mdvoxelclustering import settests as sets
+from collections import deque
+import pytest
+
+@pytest.fixture
+def olddict1():
+    cluster1 = {'a1','a2','a3','a4','a5','a6','a7','a8','a9','a10'}
+    cluster2 = {'b1','b2','b3','b4','b5','b6','b7','b8','b9','b10'}
+    cluster3 = {'c1','c2','c3','c4','c5','c6','c7','c8','c9'}
+    cluster4 = {'d1','d2','d3'}
+    cluster5 = {'e1','e2','e3','e4','e5','e6','e7','e8'}
+    cluster6 = {'f1','f2','f3','f4','f5','f6','f7','f8','f9','f10'}
+    cluster7 = {'g1','g2','g3','g4'}
+
+
+    olddict1 = {
+            0 : set(),
+            1 : cluster1,
+            2 : cluster2,
+            3 : cluster3,
+            4 : cluster4,
+            5 : cluster5,
+            6 : cluster6,
+            7 : cluster7}
+    return olddict1
+
+@pytest.fixture    
+def olddict2():
+    cluster1 = {'a1','a2','a3','a4','a5','a6','a7','a8','a9','a10'}
+    cluster2 = {'b1','b2','b3','b4','b5','b6','b7','b8','b9','b10'}
+    cluster3 = {'c1','c2','c3','c4','c5','c6','c7','c8','c9'}
+    cluster4 = {'d1','d2','d3'}
+    cluster5 = {'e1','e2','e3','e4','e5','e6','e7','e8'}
+    cluster6 = {'f1','f2','f3','f4','f5','f6','f7','f8','f9','f10'}
+    cluster7 = {'g1','g2','g3','g4'}
+    cluster8 = {'d7','d8','d9','d10','d4','d5'}
+
+    olddict2 = {
+            0 : set(),
+            1 : cluster1,
+            2 : cluster2,
+            3 : cluster3,
+            4 : cluster4,
+            5 : cluster5,
+            6 : cluster6,
+            7 : cluster7,
+            8 : cluster8   
+            }
+    return olddict2
+
+@pytest.fixture  
+def olddict3():
+    cluster1 = {'a1','a2','a3','a4','a5','a6','a7','a8','a9','a10'}
+    cluster2 = {'b1','b2','b3','b4','b5','b6','b7','b8','b9','b10'}
+    cluster3 = {'c1','c2','c3','c4','c5','c6','c7','c8','c9'}
+    cluster4 = {'d1','d2','d3'}
+    cluster5 = {'e1','e2','e3','e4','e5','e6','e7','e8'}
+    cluster6 = {'f1','f2','f3','f4','f5','f6','f7','f8','f9','f10'}
+    cluster7 = {'g1','g2','g3','g4'}
+    cluster8 = {'d7','d8','d9','d10','d4','d5'}
+    olddict3 = {
+            0 : set(),
+            1 : cluster1,
+            2 : cluster2,
+            3 : cluster3,
+            4 : cluster8,
+            5 : cluster5,
+            6 : cluster6,
+            7 : cluster7,
+            8 : cluster4   
+            }
+    return olddict3
+
+@pytest.fixture  
+def frame20():
+    cluster11 = {'a1','a2','a3','a4','a5','a6','a7','a8','a9','a10'}
+    cluster12 = {'b1','b2','b3','b4','b5'}
+    cluster13 = {'c1','c2','c3','c4','c5','c6'}
+    cluster14 = {'d1','d2','d3','d4','d5','d6','d7','d8','d9','d10'}
+    cluster15 = {'f1','f2','f3','f4','f5','f6','f7','f8','f9','f10'}
+    cluster16 = {'e1','e2','e3','e4','e5','e6','e7','e8'}
+    cluster17 = {'g1','g2','g3'}
+    cluster0 = set()
+    frame20 = deque([cluster0,cluster11,cluster12,cluster13,cluster14,cluster15,cluster16,cluster17])
+
+    return frame20
+
+@pytest.fixture  
+def frame21():
+    cluster11 = {'a1','a2','a3','a4','a5','a6','a7','a8','a9','a10'}
+    cluster12 = {'b1','b2','b3','b4','b5'}
+    cluster13 = {'c1','c2','c3','c4','c5','c6'}
+    cluster14 = {'d1','d2','d3','d4','d5','d6','d7','d8','d9','d10'}
+    cluster15 = {'f1','f2','f3','f4','f5','f6','f7','f8','f9','f10'}
+    cluster16 = {'e1','e2','e3','e4','e5','e6','e7','e8'}
+    cluster17 = {'g1','g2','g3'}
+    cluster18 = {'b7','b8','b9','b10'}
+    cluster0 = set()
+    frame21 = deque([cluster0,cluster11,cluster12,cluster13,cluster14,cluster15,cluster16,cluster17,cluster18])
+
+    return frame21
+
+@pytest.fixture  
+def frame22():
+    cluster11 = {'a1','a2','a3','a4','a5','a6','a7','a8','a9','a10'}
+    cluster12 = {'b1','b2','b3','b4','b5'}
+    cluster13 = {'c1','c2','c3','c4','c5','c6'}
+    cluster14 = {'d1','d2','d3','d4','d5','d6','d7','d8','d9','d10'}
+    cluster15 = {'f1','f2','f3','f4','f5','f6','f7','f8','f9','f10'}
+    cluster16 = {'e1','e2','e3','e4','e5','e6','e7','e8'}
+    cluster17 = {'g1','g2','g3'}
+    cluster0 = set()
+    frame22 = deque([cluster0,cluster11,cluster12,cluster13,cluster14,cluster15,cluster16,cluster17])
+
+    return frame22
+
+@pytest.fixture  
+def frame23():
+    cluster11 = {'a1','a2','a3','a4','a5','a6','a7','a8','a9','a10'}
+    cluster12 = {'b1','b2','b3','b4','b5'}
+    cluster13 = {'c1','c2','c3','c4','c5','c6'}
+    cluster14 = {'d1','d2','d3','d4','d5','d6','d7','d8','d9','d10'}
+    cluster15 = {'f1','f2','f3','f4','f5','f6','f7','f8','f9','f10'}
+    cluster16 = {'e1','e2','e3','e4','e5','e6','e7','e8'}
+    cluster17 = {'g1','g2','g3'}
+    cluster0 = set()
+    frame23 = deque([cluster0,cluster11,cluster12,cluster13,cluster14,cluster15,cluster16,cluster17])
+
+    return frame23
+
+@pytest.fixture
+def testingclusternumber():
+    testingclusternumber = 8
+    return testingclusternumber
+
+
+@pytest.fixture
+def test3deprecated():
+    test3deprecated = {8: {'d1','d2','d3'}}
+    return test3deprecated
+
+@pytest.fixture
+def deprecated_dict():
+    
+    deprecated_dict = {
+            8 : {'b7','b8','b9'}
+    }
+    return deprecated_dict
---
+++ tests/test_settests/test_settests.py
@@ -0,0 +1,36 @@
+import mdvoxelclustering as mdv
+from mdvoxelclustering import settests as sets
+from collections import deque
+
+def test_standard(olddict1, frame20):
+    result,_,_ = sets.compare_clusters(olddict1,frame20,{},8,1)
+    test0 = {0: set(), 1: {'a8', 'a5', 'a6', 'a4', 'a9', 'a2', 'a10', 'a7', 'a3', 'a1'}, 2: {'b1', 'b4', 'b3', 'b5', 'b2'}, 3: {'c4', 'c5', 'c6', 'c1', 'c2', 'c3'}, 4: {'d9', 'd5', 'd6', 'd8', 'd10', 'd2', 'd7', 'd1', 'd3', 'd4'}, 5: {'e7', 'e5', 'e6', 'e1', 'e2', 'e3', 'e8', 'e4'}, 6: {'f2', 'f1', 'f3', 'f5', 'f6', 'f7', 'f9', 'f10', 'f8', 'f4'}, 7: {'g3', 'g1', 'g2'}}
+    assert(result == test0), "Standard merging failed"    
+
+
+  
+def test_newcreate(olddict1,frame21):    
+    result,resultnumber,_ = sets.compare_clusters(olddict1,frame21,{},8,1)
+    test1 = {0: set(), 1: {'a8', 'a5', 'a6', 'a4', 'a9', 'a2', 'a10', 'a7', 'a3', 'a1'}, 2: {'b1', 'b4', 'b3', 'b5', 'b2'}, 3: {'c4', 'c5', 'c6', 'c1', 'c2', 'c3'}, 4: {'d9', 'd5', 'd6', 'd8', 'd10', 'd2', 'd7', 'd1', 'd3', 'd4'}, 5: {'e7', 'e5', 'e6', 'e1', 'e2', 'e3', 'e8', 'e4'}, 6: {'f2', 'f1', 'f3', 'f5', 'f6', 'f7', 'f9', 'f10', 'f8', 'f4'}, 7: {'g3', 'g1', 'g2'}, 9: {'b8', 'b9', 'b7', 'b10'}}
+    assert (result == test1 and resultnumber == 9), "Create new cluster failed" 
+  
+def test_recreate(olddict1,frame21,deprecated_dict):
+    
+    result,resultnumber,resultdep =sets.compare_clusters(olddict1,frame21,deprecated_dict,8,1)
+
+    test4 = {0: set(), 1: {'a8', 'a5', 'a6', 'a4', 'a9', 'a2', 'a10', 'a7', 'a3', 'a1'}, 2: {'b1', 'b4', 'b3', 'b5', 'b2'}, 3: {'c4', 'c5', 'c6', 'c1', 'c2', 'c3'}, 4: {'d9', 'd5', 'd6', 'd8', 'd10', 'd2', 'd7', 'd1', 'd3', 'd4'}, 5: {'e7', 'e5', 'e6', 'e1', 'e2', 'e3', 'e8', 'e4'}, 6: {'f2', 'f1', 'f3', 'f5', 'f6', 'f7', 'f9', 'f10', 'f8', 'f4'}, 7: {'g3', 'g1', 'g2'}, 8: {'b8', 'b9', 'b7', 'b10'}}
+
+    assert (result == test4 and resultnumber == 8 and resultdep == {}), "Recreate old cluster failed"
+   
+def test_merge(olddict2,frame22):
+    result,_,_ = sets.compare_clusters(olddict2,frame22,{},8,1)
+    test2 = {0: set(), 1: {'a8', 'a5', 'a6', 'a4', 'a9', 'a2', 'a10', 'a7', 'a3', 'a1'}, 2: {'b1', 'b4', 'b3', 'b5', 'b2'}, 3: {'c4', 'c5', 'c6', 'c1', 'c2', 'c3'}, 5: {'e7', 'e5', 'e6', 'e1', 'e2', 'e3', 'e8', 'e4'}, 6: {'f2', 'f1', 'f3', 'f5', 'f6', 'f7', 'f9', 'f10', 'f8', 'f4'},  7: {'g3', 'g1', 'g2'}, 8: {'d1','d2','d3','d4','d5','d6','d7','d8','d9','d10'}}
+    assert(result == test2), "Merging clusters failed" 
+     
+    
+def test_deprecation(olddict3,frame23,test3deprecated):
+    result,_,resultdep = sets.compare_clusters(olddict3,frame23,{},8,1)
+    test3 = {0: set(), 1: {'a8', 'a5', 'a6', 'a4', 'a9', 'a2', 'a10', 'a7', 'a3', 'a1'}, 2: {'b1', 'b4', 'b3', 'b5', 'b2'}, 3: {'c4', 'c5', 'c6', 'c1', 'c2', 'c3'}, 4: {'d9', 'd5', 'd6', 'd8', 'd10', 'd2', 'd7', 'd1', 'd3', 'd4'}, 5: {'e7', 'e5', 'e6', 'e1', 'e2', 'e3', 'e8', 'e4'},  6: {'f2', 'f1', 'f3', 'f5', 'f6', 'f7', 'f9', 'f10', 'f8', 'f4'}, 7: {'g3', 'g1', 'g2'}}
+    assert(result == test3 and resultdep == test3deprecated), "Deprecating cluster failed"
+
+
