2dbd93d6dd1f95792d11d8b2a7fd227dae2fa496
AlbertThie <alb0rt@gmail.com>
2019-07-09 21:23:43 +0200

    show overtime cluster changes

--- mdvoxelclustering/settests.py
+++ mdvoxelclustering/settests.py
@@ -8,6 +8,7 @@ Created on Tue Mar  5 12:17:03 2019
 from collections import deque
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
 
 identity_threshold = 0.0000001
 cluster_mutations = {}
@@ -85,88 +86,6 @@ def compare_clusters (clustersold,clustersnew,deprecated_clusters,clusternumber,
                 add_cluster_change(frame_number,(newkey,index,"c"))
                
     return output,clusternumber, deprecated_clusters  
-#        
-#        
-#        
-#        if index in mergelist:
-#            if mergelist[index][0] > len(value):
-#                print("cluster " + str(key) + " was merged into cluster " + str(mergelist[index][1]) + " at frame " + str(frame_number) + "  1")   
-#                add_cluster_change(frame_number,(key,mergelist[index][1],"m"))
-#                deprecated_clusters[key] = value
-#            else:
-#                deprecated_clusters[mergelist[index][1]] = output[mergelist[index][1]]
-#                print("cluster " + str(mergelist[index][1]) + " was merged into cluster " + str(key) + " at frame " + str(frame_number) + "  2")
-#                add_cluster_change(frame_number,(mergelist[index][1],key,"m"))
-#           
-#
-#
-#        else:
-#            mergelist[index] = (len(value),key)
-#            output[key] = clustersnew[index]
-#
-#    return output,clusternumber,deprecated_clusters
-#else:
-#    for key,value in clustersold.items():
-#        likeness_score=index=0
-#           # if not clustersnew:
-#                 # If there are still old clusters, but no more new, check which two clusters have merged by comparing each cluster.
-#            #     for key_out,value_out in output.items():
-#            #         temp_score = len(value.intersection(value_out))/max(len(value),len(value_out)) 
-#            #         if temp_score > likeness_score:
-#            #                likeness_score = temp_score
-#            #                index = key_out
-#            #     if len(value) > len(value_out):
-#            #         output[key] = value 
-#            #         del output[index]
-#            #         deprecated_clusters[index] = value_out
-#            #         print("cluster " + str(index) + " was merged into cluster " + str(key) + " at frame " + str(frame_number))
-#            #         add_cluster_change(frame_number,(index,key,"m"))
-#            #     else:
-#            #         print("cluster " + str(key) + " was merged into cluster " + str(index) + " at frame " + str(frame_number))   
-#            #         add_cluster_change(frame_number,(key,index,"m"))
-#             #        deprecated_clusters[key] = value
-#            #else:
-#            for idx, clusternew in enumerate(clustersnew):
-#                temp_score = len(value.intersection(clusternew))/max(len(value),len(clusternew)) 
-#                if temp_score > likeness_score:
-#                    likeness_score = temp_score
-#                    index = idx
-#
-#            clustersnew.rotate(-index)
-#            output[key] = clustersnew.popleft()
-#        while clustersnew:
-#            likeness_score=index=0
-#            for key,value in clustersold.items():
-#                temp_score = len(value.intersection(clusternew))/max(len(value),len(clusternew)) 
-#                if temp_score > likeness_score:
-#                    likeness_score = temp_score
-#                    index = key
-#            likeness_score = 0
-#            remerge = False
-#            if len(clusternew) > len(output[index]):
-#                clusternew, output[index] = output[index],
-#
-#            for key, value in deprecated_clusters.items():
-#                
-#                temp_score = len(value.intersection(clusternew))/max(len(value),len(clusternew)) 
-#                if (temp_score > identity_threshold) and (temp_score > likeness_score) :
-#                    likeness_score = temp_score
-#                    index_old = key
-#                    remerge = True
-#                
-#            if remerge:
-#                output[index_old] = clustersnew.popleft()
-#                print ("cluster " + str(index_old) + " was recreated from cluster " + str(index) + " at frame " + str(frame_number))
-#                add_cluster_change(frame_number,(index_old,index,"rc"))
-#                del deprecated_clusters[index_old]
-#            else:
-#                newkey = clusternumber + 1
-#                clusternumber = newkey
-#                output[newkey ] = clustersnew.popleft()
-#                print ("cluster " + str(newkey) + " was created from cluster " + str(index) + " at frame " + str(frame_number))
-#                add_cluster_change(frame_number,(newkey,index,"c"))
-#        return output,clusternumber,deprecated_clusters
-
 
 def sort_clusters(data):
     clustercount = 0
@@ -193,9 +112,13 @@ def main():
     data, plotinfo = sort_clusters(data)
     
     np.save('clusters_ordered', data)
-    np.save('Visualization_data',plotinfo)
-    np.save('Cluster_mutations',cluster_mutations)
     
+    with open('visualization_data.pickle', 'wb') as handle:
+        pickle.dump(plotinfo, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+    with open('cluster_mutations.pickle', 'wb') as handle:
+        pickle.dump(cluster_mutations, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
     time = range(len(plotinfo))
     ordered_clusters = plotinfo.values()
     print(time)
